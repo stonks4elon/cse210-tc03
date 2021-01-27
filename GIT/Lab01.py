91251d52f00023268e986825f928567af4a066d9
Bryson <bryson.berrett19@.com>
2021-01-27 13:52:49 -0700

    this includes my last name

---
+++ GIT/Lab01.py
@@ -0,0 +1,36 @@
+# 1. Name: 
+#    Bryson Berrett
+# 2. Assignment Name: 
+#    Lab 01: Python Review
+# 3. Assignment Description: 
+#    This is a number guessing game.
+# 4. What was the hardest part? Be as specific as possible.
+#    -a paragraph or two about how the assignment went for you-
+# 5. How long did it take for you to complete the assignment?
+#    -total time in hours including reading the assignment and submitting the program-  
+
+import random
+
+# Game introduction
+msg "This is a 'guess a number' game"
+    "You try to guess a randome number in the smallest number of attempts."
+    "You were able to guess a number in" 
+# Prompt the user for how difficult the game will be. Ask for an integer
+value_max = input("Pick a positive integer")
+print(value_max)
+# Generate a random number between 1 and the chosen value
+ value_random = random.randint(1, value_max)
+
+# Give the user instructions as to what he or she will be doing
+
+# Initialize the sentinal and the array of guesses
+
+# Play the guessing game
+
+    # Prompt the user for a number
+
+    # Store the number in an array so it can be displayed later
+
+    # Make a decision: was the guess too high, too low, or just right
+
+# Give the user a report: How many guesses and what the guesses where
